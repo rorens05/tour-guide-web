64cda8a4dd84855071c14f74a837294a2c7e19ff
Laurence Bautista <rorens05@gmail.com>
2021-09-30 22:07:34 +0800

    Add login for end user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
+
   def index;
-  redirect_to admin_dashboard_path
+  # redirect_to admin_dashboard_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,7 @@ Rails.application.routes.draw do
   get 'profile', to: 'profile#index'
   get 'profile/change_password'
   patch 'profile/update_password'
-  # devise_for :users
+  devise_for :users
   get 'users/:id' => 'users#show'
   get 'home/index'
   root 'home#index'
@@ -96,61 +96,6 @@ Rails.application.routes.draw do
           get :my_prices
         end
       end
-
-    end
-    namespace :v1 do
-      get 'auth/connection_test'
-      post 'auth/login'
-      post 'auth/login_with_facebook'
-      post 'auth/login_with_google'
-      post 'auth/login_with_apple'
-      post 'auth/register_user'
-      post 'auth/register_playmate'
-      post 'auth/register_as_merchant'
-      post 'auth/verify_code'
-      post 'auth/resend_verification_code'
-      post 'auth/forgot_password'
-      post 'auth/verify_forgot_password_code'
-      post 'auth/change_password'
-      post 'auth/resend_forgot_password_code'
-      post 'auth/broadcast_logout'
-      post 'auth/send_confirmation_email'
-      get 'auth/tokens'
-      get 'auth/profile'
-
-      post 'game/create_game'
-      get 'game/join_game'
-      post 'game/spin_game'
-      post 'game/broadcast_game'
-      post 'game/override_timer'
-      get 'game/:id', to: 'game#show'
-
-      resources :notifications do
-        collection do
-          get :in_app_notifications
-        end
-        member do
-          post :read
-        end
-      end
-
-      resources :mobile_releases do
-        collection do
-          get :check_for_updates
-          post :register_new_install
-        end
-      end
-
-      resources :stations
-      
-      resources :documents do
-        collection do
-          get :terms_and_conditions
-          get :privacy_policy
-        end
-      end
-
-      resources :quiz_games 
     end
   end
 end
